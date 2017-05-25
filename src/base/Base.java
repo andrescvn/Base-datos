e340be4387896a7e3f2fcd6da694ccc8e3c1ad4d
acomesanavila <acomesanavila@nautilus7.danielcastelao.org>
2017-05-25 09:39:31 +0200

    fin

--- src/base/Base.java
+++ src/base/Base.java
@@ -18,33 +18,38 @@ public class Base {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int op;
-        Metodos met = new Metodos();
-        met.conexion();
-        do{
-        op=Integer.parseInt(JOptionPane.showInputDialog("1)Conectar con base de datos \n 2)Crear tabla \n 3)Introducir datos \n4)Busqueda \n5)Cambio de datos \n6)Borrado \n7)Salir"));
-        
-        switch(op){
-            case 1:
-                met.crearTabla();
-                break;
-            case 2:
-                met.insertarDatos();
-                break;
-            case 3:
-                met.busqueda();
-                break;
-            case 4:
-                met.update();
-                break;
-            case 5:
-                met.delete();
-                break;
-            case 6:
-                met.cerrarConexion();
-                System.exit(0);
-        }
-        }while(op!=6);
-
+//        int op;
+      Metodos met = new Metodos();
+//        met.conexion();
+//        do{
+//        op=Integer.parseInt(JOptionPane.showInputDialog("1)Conectar con base de datos \n 2)Crear tabla \n 3)Introducir datos \n4)Busqueda \n5)Cambio de datos \n6)Borrado \n7)Salir"));
+//        
+//        switch(op){
+//            case 1:
+//                met.crearTabla();
+//                break;
+//            case 2:
+//                met.insertarDatos();
+//                break;
+//            case 3:
+//                met.busqueda();
+//                break;
+//            case 4:
+//                met.update();
+//                break;
+//            case 5:
+//                met.delete();
+//                break;
+//            case 6:
+//                met.cerrarConexion();
+//                System.exit(0);
+//        }
+//        }while(op!=6);
+//
+//}
+met.conexion();
+//met.insertarDatos();
+Tabla t= new Tabla();
+t.setVisible(true);
 }
 }
